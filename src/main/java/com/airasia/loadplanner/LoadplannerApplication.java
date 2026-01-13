79f67e096a5284f60c47c36f3db18844411b15ad
Md. Zahid Hasan <mzhj19@gmail.com>
2026-01-13 21:56:06 +0600

    initial commit

---
+++ src/main/java/com/airasia/loadplanner/LoadplannerApplication.java
@@ -0,0 +1,13 @@
+package com.airasia.loadplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airasia/loadplanner/LoadplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airasia.loadplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
